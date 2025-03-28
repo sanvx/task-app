4e373532238dd01ee4a6bcb7946d87d7d6330977
Daniil Pak <sanva970@gmail.com>
2025-03-28 09:45:11 +0200

    added not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-gray-800">
+      <h1 className="text-5xl font-bold">404</h1>
+      <p className="text-xl mt-2">Page not found</p>
+      <p className="mt-4 text-gray-600 dark:text-gray-400">Use the header to navigate</p>
+    </div>
+  );
+}
